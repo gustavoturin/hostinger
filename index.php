13d34f82b0028c898aeac1fedad5b4657b4840a2
Gustaturin <73546747+gustavoturin@users.noreply.github.com>
2024-08-01 13:40:50 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Resultado da Conexão com o Banco de Dados</title>
+    <title>Conexão</title>
     <style>
         body {
             font-family: Arial, sans-serif;
